28cb3d433d38386bab5f717b63f8c970785a01f4
Ramiro Quiroga <ramiro.quiroga@cepsi.net.ar>
2025-07-01 11:32:48 -0300

    agregando dominio controstock.online a confi vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,10 +22,13 @@ export default defineConfig({
     mode: 'standalone',
   }),
   vite: {
+    server: {
+      allowedHosts: ['controlstock.online'],
+    },
     resolve: {
       alias: {
-        '@': '/src'
-      }
-    }
-  }
+        '@': '/src',
+      },
+    },
+  },
 });
